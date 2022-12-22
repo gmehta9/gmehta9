e2cd05c1eb17d91cc9fcd650be0947491bcf235f
gmehta9 <55479732+gmehta9@users.noreply.github.com>
2022-12-22 10:06:02 +0530

    js

---
+++ JSScript.js
@@ -0,0 +1,14 @@
+    // JS file load method  
+    function loadScript(src) {
+        return new Promise((resolve) => {
+            const script = document.createElement("script");
+            script.src = src;
+            script.onload = () => {
+                resolve(true);
+            };
+            script.onerror = () => {
+                resolve(false);
+            };
+            document.body.appendChild(script);
+        });
+    }
